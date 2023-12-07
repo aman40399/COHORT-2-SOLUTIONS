f498a81b07fb1626f010395c890865794a9ad2da
Aman Singh <146389653+aman40399@users.noreply.github.com>
2023-12-08 05:21:39 +0530

    strctural changes (into week file making easy to naviaget)

--- 1.3.js
+++
@@ -1,7 +0,0 @@
-//basic programs js
-//write a program to greet a person given there first and last name
-
-var xname= prompt("enter your name");
-var last_name=prompt("enter your last name");
-console.log( xname +" "+ last_name+" "+"welcom to cohort");
-
